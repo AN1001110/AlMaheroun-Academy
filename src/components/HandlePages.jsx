0493e66d0b98fec6f217b2525d0a509f4ad1c849
AN <drgoncom8@gmail.com>
2025-06-20 05:25:55 +0300

    fix small spelling wrong

--- src/components/HandlePages.jsx
+++ src/components/HandlePages.jsx
@@ -5,11 +5,11 @@ export default function ScrollToTop() {
   const titles = {
     "/": "أكاديمية الماهرون",
     "/programs": "البرامج",
-    "/about": "من نحن",
-    "/contact": "اتصل بنا",
+    "/about": "عن الأكاديمية",
+    "/contact": "التواصل",
     "/teachers": "المعلمون",
   };
-  
+
   document.title = titles[location.pathname] || "أكاديمية الماهرون";
   const { pathname } = useLocation();
 
